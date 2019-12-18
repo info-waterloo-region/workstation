0b9bae6598004f424b32d98c46a19c2e012fbcdd
Todd Turnbull <shingaling@gmail.com>
2019-12-18 12:40:37 -0500

    Update setup.sh based on Ansible documentation

--- setup.sh
+++ setup.sh
@@ -2,5 +2,6 @@
 
 sudo apt update
 sudo apt upgrade -y
-sudo apt install -y --no-upgrade ansible git
+sudo apt-add-repository --yes --update ppa:ansible/ansible
+sudo apt install -y --no-upgrade software-properties-common ansible git
 
